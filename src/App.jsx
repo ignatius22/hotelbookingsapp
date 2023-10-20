77d55e65d08e307028450428c247556df202e727
ignatius22 <igee54077@gmail.com>
2023-10-20 09:22:55 +0100

    Create useable heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalSyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  color: red;
-  background-color: black;
-`;
-
-const Button = styled.button`
-  font-size: 23px;
-  padding: 5px 20px;
-  background-color: green;
-  color: white;
-  border-radius: 7px;
-  border: none;
+const StyleApp = styled.main`
+  background-color: var(--color-grey-200);
+  padding: 20px;
 `;
 
 function App() {
   return (
     <div>
-      <H1>The Wild Oasis</H1>
-      <Button>Checkin</Button>
-      <Button style={{ backgroundColor: "black" }}>Checkin</Button>
+      <GlobalStyles />
+      <StyleApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+
+        <Heading as="h2">Book Now</Heading>
+        <Button>Checkin</Button>
+        <Button style={{ backgroundColor: "black" }}>Checkin</Button>
+      </StyleApp>
     </div>
   );
 }
