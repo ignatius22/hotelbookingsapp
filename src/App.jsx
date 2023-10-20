b510e1aae7e2cd6a6e9436bb79d954034aa71be7
ignatius22 <igee54077@gmail.com>
2023-10-20 08:04:26 +0100

    Add global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import styled from "styled-components";
 
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: red;
+  background-color: black;
+`;
+
+const Button = styled.button`
+  font-size: 23px;
+  padding: 5px 20px;
+  background-color: green;
+  color: white;
+  border-radius: 7px;
+  border: none;
+`;
 
 function App() {
   return (
-    <div>Hello World</div>
-  )
+    <div>
+      <H1>The Wild Oasis</H1>
+      <Button>Checkin</Button>
+      <Button style={{ backgroundColor: "black" }}>Checkin</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
