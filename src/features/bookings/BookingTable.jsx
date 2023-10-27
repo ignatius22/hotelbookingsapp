29908ee0eb4c68471e7cd6faa94f88f04388a938
ignatius22 <igee54077@gmail.com>
2023-10-27 15:31:46 +0100

    Integrate the booking detail page

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,11 +4,12 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings , isLoading} = useBookings();
-
-  if(isLoading) return <Spinner/>
+  const { bookings, isLoading, count } = useBookings();
+console.log(count)
+  if (isLoading) return <Spinner />;
   if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
@@ -29,6 +30,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination  count={count}/>
+        </Table.Footer>
       </Table>
     </Menus>
   );
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -11,7 +11,6 @@ function Bookings() {
         <BookingTableOperations />
       </Row>
 
-   
       <BookingTable />
     </>
   );
