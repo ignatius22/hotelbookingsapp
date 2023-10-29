3074c142634ae4bac6213232f98ed30da2f43910
ignatius22 <igee54077@gmail.com>
2023-10-29 10:04:46 +0100

    Add authorization to the app and protected routes feature

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,9 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) =>
@@ -12,8 +13,8 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
-      console.log(user);
       navigate("/dashboard");
+      queryClient.setQueriesData(["user"], user);
       toast.success("User logged in successfully");
     },
     onError: (err) => {
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  // 1. Load the authenticated user
+  const { isAuthenticated, isLoading } = useUser();
+
+  //2. If there's no authenticated user, redirct to the login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) return navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  //3. while loading show spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // if there is a user render the app
+
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
